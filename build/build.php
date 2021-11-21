a686b429f023c6e31ae91a8e51fd9353847fc04e
Arrtist <av.arrtist@gmail.com>
2021-11-21 19:57:48 +0300

    shared min.js for upcoming vue3 support

---
+++ build/build.php
@@ -0,0 +1,9 @@
+<?php
+
+$file1 = file_get_contents('./dist/single-file-upload-for-vue.min.js');
+$file2 = file_get_contents('./dist/vue3/single-file-upload-for-vue.min.js');
+$file1 = str_replace('var SingleFileUploadForVue=', "var SingleFileUploadForVue=Vue.version[0]==='3'?", $file1);
+$file1 = substr($file1, 0, -1).':';
+$file2 = str_replace('var SingleFileUploadForVue=', '', $file2);
+
+file_put_contents('./dist/min.js', $file1 . $file2);
